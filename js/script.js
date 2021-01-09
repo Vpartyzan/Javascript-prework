e641320a660ba3444b51e99a9f39d2c3acb2153f
Maks <peregrim.mt@gmail.com>
2021-01-09 13:22:38 +0100

    Add game ending logic in script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,21 @@ if (playerInput == '1') {
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Komputer wygrał');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Komputer wygrał');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Komputer wygrał');
+} else if (computerMove == playerMove) {
+    printMessage('Remis');
+} else if (playerMove == 'nieznany ruch') {
+    printMessage('Bląd, Ty wybrałeś nieznany ruch');
+}
